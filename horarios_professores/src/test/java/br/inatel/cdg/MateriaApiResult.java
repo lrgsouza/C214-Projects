a76848d26a8cf5f531f1f99b3cef0ba8ce5dea94
diogocastros1 <diogoacsantos93@gmail.com>
2024-03-23 14:41:32 -0300

    test: :construction: include api results for materia

---
+++ horarios_professores/src/test/java/br/inatel/cdg/MateriaApiResult.java
@@ -0,0 +1,24 @@
+package br.inatel.cdg;
+
+public class MateriaApiResult {
+
+    public static String C214 = 
+    "{\n" + 
+    "    \"nomeDaMateria\": \"Engenharia de Produto de Software\",\n" + 
+    "    \"periodo\": \"Integral\",\n" +
+    "    \"salas\": [\"1\",\"2\",\"3\"]\n" +
+    "    \"horarios\": [\"8:30\",\"19:30\"]\n" + 
+    "    \"professores\": [\"Chris Lima\"]\n" + 
+    "}";
+    public static String C111 = 
+    "{\n" + 
+    "    \"nomeDaMateria\": \"Analise de dados\",\n" + 
+    "    \"periodo\": \"Noturno\",\n" +
+    "    \"salas\": [\"1\",\"2\",\"3\"]\n" +
+    "    \"horarios\": [\"19:30\"]\n" + 
+    "    \"professores\": [\"Renzo Mesquita\"]\n" + 
+    "}";
+    
+
+}
+
